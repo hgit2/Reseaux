df7aa83a3a23827b582d90e8235e518659eef911
hgit2 <hana.lbath@insa-lyon.fr>
2019-03-14 13:37:50 +0100

    version de lundi

---
+++ TP_reseaux_v3ancetre/clientMulti_ecriture_v2.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#                                                                Projet Réseaux 4BIM
+#                                                                    Code client
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# Imports généraux :
+import socket 
+import select
+import time
+import sys
+import os
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+global plot_dispo 
+plot_dispo=False # à gérer PLUS TARD
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+def creation_repertoire(des,s):
+    premiere_analyse=True
+    try:
+        os.mkdir("Analyse_"+des) # Permet de tester si le dossier '"Analyse_"+des' existe.
+    except FileExistsError:
+        premiere_analyse=False # Si le dossier existe deja alors l'analyse de la sequence entree existe deja, on ne souhaite pas la refaire inutilement.
+        print(" \nL'analyse de cette sequence a deja ete effectuee, vous pouvez \napprofondir cette analyse ou effectuer une annalyse sur une nouvelle sequence. \n")
+    os.chdir("./Analyse_"+des) # Si le dossier existe deja il n'est pas cree et on rentre simplement dedans, sinon il a deja ete creer dans le 'try' et donc on rentre dedans.
+    premiere_analyse=str(premiere_analyse)
+   # print("Premier analyse : %s"%premiere_analyse)
+    premiere_analyse=premiere_analyse.encode()
+    s.sendall(premiere_analyse)
+    s.recv(255).decode()
+
+
+
+def creation_fichier(nom_fichier) :
+    nom_fichier=nom_fichier.replace("\n","")
+    fichier_existe=True # Variable permettant de verifier que le fichier qu'on va creer n'en ecrase pas un preexistant.
+    numero_fichier=0
+    while fichier_existe: # Tant que le fichier "nom_fichier.png" existe le nom change.
+        try:
+            sortie=open(nom_fichier+"(%i).txt" % numero_fichier,'r') # Test si le fichier "nom_fichier.py" existe.
+        except FileNotFoundError:
+            fichier_existe=False
+        else:
+            sortie.close()
+            numero_fichier+=1
+            nom_fichier=nom_fichier.replace("(%i)" % (numero_fichier-1),"(%i)" % numero_fichier)  
+    return(nom_fichier, numero_fichier)
+
+
+
+def ecriture_adn(s) :
+    nom=s.recv(1024).decode()
+    nom_fichier, numero_fichier = creation_fichier(nom)
+    s.sendall("OK".encode())
+    sortie=open(nom_fichier+"(%i).txt" % numero_fichier,'a') # Ouverture du fichier resultat.
+    message=s.recv(1024).decode()
+    if len(message.split('\n'))>1: #message.split('\n')[1] == "Attention : Execution incomplete du programme.":
+        print(message)
+        s.sendall("OK".encode())
+        size=s.recv(1024).decode()
+    else :
+        size=message
+    s.sendall("OK".encode())
+    file=s.recv(int(size)).decode()
+    sortie.write(file)  
+    sortie.close()
+    #print ("Results are available in {0}({1})".format(nom_fichier, numero_fichier))
+    print ("Results are available in "+nom_fichier+"(%i).txt"% numero_fichier)
+    print(s.recv(1024).decode())
+
+
+
+def ecriture_proteine(s) :
+    nom=s.recv(1024).decode()
+    print("nom:%s"%nom)
+    nom_fichier, numero_fichier = creation_fichier(nom)
+    s.sendall("OK".encode())
+    sortie=open(nom_fichier+"(%i).txt" % numero_fichier,'a') # Ouverture du fichier resultat.
+    message=s.recv(1024).decode()
+    if len(message.split('\n'))>1: #message.split('\n')[1] == "Attention : Execution incomplete du programme.":
+        print(message)
+        s.sendall("OK".encode())
+        size=s.recv(1024).decode()
+    else :
+        size=message
+    s.sendall("OK".encode())
+    file=s.recv(int(size)).decode()
+    sortie.write(file)  
+    sortie.close()
+    #print ("Results are available in {0}({1})".format(nom_fichier, numero_fichier))
+    print ("Results are available in "+nom_fichier+"(%i).txt"% numero_fichier)
+    print(s.recv(1024).decode())
+
+
+
+def envoie_fasta(description,seq) :
+    "Cette fonction envoie au serveur des données fasta stockées en local."
+    if description=="entree":
+        description= description.encode()
+        s.sendall(description)
+        rep = s.recv(2).decode()
+    else:
+        description= description.encode()
+        seq = seq.encode()
+        s.sendall(description)
+        rep = s.recv(2).decode() # Le serveur a bien recu la description
+        taille=str(len(seq))
+        s.sendall(taille.encode())
+        rep = s.recv(2).decode() # Le serveur a bien recu la description
+        #print("TAILLE BIEN RECU ")
+        s.sendall(seq)
+
+
+    
+def lecture_fasta_loc(adresse) :
+    "Cette fonction permet de recuperer une sequence et sa description dans un fichier place dans le repertoire courant grace a son nom donne en argument (entre guillemets, sans oublier l'extension)."
+    try:
+        sequence=""
+        fasta=open(adresse,"r") # Permet d'ouvrir un fichier existant pour lire les informations qu'il contient.
+        ligne=fasta.readline() # Affecte a la variable ligne la premiere ligne de "fasta" sous forme de chaine de caratere.
+        print(ligne)
+        while ligne[0] != ">": # Permet de recuperer la description de la sequence qui toujours est precedee de ">" au format fasta.
+            ligne=fasta.readline()
+        description=ligne[1:].strip()
+        while ligne != "": # Permet de recuperer la sequence et de s'arreter lorque la lecture a atteint la findu fichier.
+            ligne=fasta.readline().strip()
+            sequence+=ligne
+        fasta.close() # Permet de refermer le fichier "fasta" ouvert au debut de la fonction. 
+    except FileNotFoundError : # Cette erreur remonte si le fichier dont l'adresse est donnee en entree n'existe pas dans l'emplacement du module. 
+        print("\n----------------\nAttention :\n\nLe fichier est introuvable. Verifiez qu'il n'y a pas de fautes de frappe.\n")
+        print("Attention : Relance du programme\n---------------\n")
+        description,sequence="entree",""
+    return(description,sequence)
+
+
+
+#creation de la socket puis connexion
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1",int(sys.argv[1])))
+
+print("Pour fermer cette session, veuillez écrire exit()\n")
+# Créer un argument pour l'adresse ip ?
+print("Connection on {}\n".format(sys.argv[1]))
+
+while 1:  
+    data = s.recv(1024).decode()
+    sdata=data.split("__") # permet de parser data
+    msg="" # initialisation du message qui sera envoyé par le client
+    
+    if sdata[0]=="ERROR":
+        # gestion des erreurs
+        print(sdata[1])
+        msg="OK"
+        
+    elif data=="resultat_prot":
+        s.sendall("OK".encode())
+        print("Analyse en cours...\n")
+        ecriture_proteine(s)
+
+    elif data=="resultat_adn":
+        s.sendall("OK".encode())
+        print("Analyse en cours...\n")
+        ecriture_adn(s)
+        
+    elif data.split(":")[0]=="creation dossier":
+        des=data.split(":")[1]
+        creation_repertoire(des,s)
+ #       s.sendall("OK".encode())
+        continue
+    
+    elif data=="nouvelle analyse":
+        os.chdir("./..")
+        s.sendall("OK".encode())
+        continue
+    
+    else :
+        #print('if not resultats_prot the variable data is  :  ' ,data) # on affiche la reponse
+        print(data)
+    
+    if msg=="OK":
+        # alors on a traité une erreur
+        s.sendall(msg.encode())
+        print("OK envoye")
+    else:
+        msg = input('>> ')
+    
+        # test pour arreter le client python proprement
+        if msg=="exit()": # si on initialise pas msg avec raw_input : comme on utilise NC et pas telnet sur les machines BIM il faut mettre if msg=="\n" pour que ca fonctionne 
+            # mais la comme on initialise raw_input c'est bon puisque raw_input renvoi une chaine vide quand on tape entree
+            break
+        elif msg=="":
+            s.sendall("WARNING : empty message".encode())
+            
+        elif "." in msg:
+            print("Traitement d'une séquence fasta en locale...")
+            s.sendall(msg.encode())
+            file_name = msg
+            description,seq = lecture_fasta_loc(file_name)
+            envoie_fasta(description,seq)
+        else:        
+        # envoi puis reception de la reponse
+            s.sendall(msg.encode())
+            print("after sendall")
+
+# fermeture de la connexion
+s.close()
+print("fin du client TCP")
+
+#if __name__=="__main__":
+
+
---
+++ TP_reseaux_v3ancetre/lire_fasta.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#                                                                     Projet Réseaux 4BIM
+#                                                        Lecture de fichier fasta et de fiche fasta en ligne
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+import urllib.request
+import urllib.error 
+
+def lire_fasta(adresse): # Recupere une sequence dans un fichier au format fasta place dans le meme dossier que ce module grace au nom complet du fichier.
+    "Cette fonction permet de recuperer une sequence et sa description dans un fichier place dans le repertoire courant grace a son nom donne en argument (entre guillemets, sans oublier l'extension)."
+    sequence=""
+    fasta=open(adresse,"r") # Permet d'ouvrir un fichier existant pour lire les informations qu'il contient.
+    ligne=fasta.readline() # Affecte a la variable ligne la premiere ligne de "fasta" sous forme de chaine de caratere.
+    while ligne[0] != ">": # Permet de recuperer la description de la sequence qui toujours est precedee de ">" au format fasta.
+        ligne=fasta.readline()
+    description=ligne[1:].strip()
+    while ligne != "": # Permet de recuperer la sequence et de s'arreter lorque la lecture a atteint la findu fichier.
+        ligne=fasta.readline().strip()
+        sequence+=ligne
+    fasta.close() # Permet de refermer le fichier "fasta" ouvert au debut de la fonction. 
+    return(description,sequence)
+
+
+def lire_fasta_web(adresse,type_seq,con): # Recupere une sequence fasta sur internet grace a son identifiant entre guillemets et au type de la sequence ("prot" ou "nucl").
+    "Cette fonction permet de recuperer une sequence et sa decription dans une fiche fasta en ligne grace a son identifiant donne en premier argument et au type de sequence donne en deuxieme argument ('prot' ou 'nucl')."
+    adresse=adresse.upper()
+    if type_seq=="prot":
+        url="http://www.uniprot.org/uniprot/"+adresse+".fasta"
+    else:
+        url="http://www.ebi.ac.uk/ena/data/view/"+adresse+"&display=fasta"
+    try:
+        u=urllib.request.urlopen(url) # Ouvre la page internet correspondant a l'url determine ci-dessus.
+    except urllib.error.HTTPError : # Si le lien internet n'existe pas.
+        print("http error")
+        con.sendall("ERROR__\n----------------\nAttention : Le lien est introuvable\nVerifiez qu'il n'y a pas de faute de frappe\nou que vous n'avez pas oublie l'extention du fichier.\nSinon verifiez que l'identifiant correspond bien a une sequence du type : {}eique.\nVeuillez modifiez vos entrees en consequence. \nAttention : Relance du programme\n---------------\n".format(type_seq).encode())
+        ok=con.recv(1024).decode() # réception de l'accusé réception du client
+        print(ok)
+        return "error", "error"
+    except urllib.error.URLError : # Si la connexion internet ne fonctionne pas.
+        print("url error")
+        con.sendall("ERROR__\n----------------\nAttention : Impossible d'acceder a la base de donnees en ligne.\nVerifiez que vous avez bien une connnection internet active sur ce poste.\nAttention : Relance du programme\n---------------\n".encode())
+        ok=con.recv(1024).decode() # réception de l'accusé réception du client
+        print(ok)
+        return "error", "error"
+    except UnicodeEncodeError : # Si l'identifiant contient des caracteres speciaux non reconnus (accents, guillemets...). 
+        print("unicode error")
+        con.sendall("ERROR__\n----------------\nAttention : L'identifiant entre est incorecte\nVerifiez qu'il n'y a pas de faute de frappe,d'espaces\nou que vous n'avez pas oublie l'extention du fichier\nVeuillez modifiez vos entrees en consequence.\nAttention : Relance du programme\n---------------\n".encode())
+        ok=con.recv(1024).decode() # réception de l'accusé réception du client
+        print(ok)
+        return "error", "error"
+#    else:
+#        print("else")
+#        con.sendall("--------------\nAttention : Il y a eu une erreur\nAttention : Relance du programme\n---------------\n".encode())
+#        return "error", "error"
+    sequence=""
+    ligne=u.readline().strip().decode("utf8") # Enleve les eventuels espaces en debut et fin de chaine de caractere et decode la ligne internet qui est codee en "utf8".
+    if ligne != "Entry: %s display type is either not supported or entry is not found." %adresse and ligne!="": 
+        while ligne[0] != ">": 
+            ligne=u.readline().strip().decode("utf8")             
+        description=ligne[1:]
+        while ligne != "":
+            ligne=u.readline().strip().decode("utf8") 
+            sequence=sequence+ligne
+        u.close() # Referme la page internet ouverte. 
+    else : # La page touvee indique que la sequence demandee n'est pas referencee ou introuvable.
+        print("seq introuvable")
+        description="La sequence n'est pas referencee."
+    return(description,sequence)
+
